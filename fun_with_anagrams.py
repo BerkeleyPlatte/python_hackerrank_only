214e4d529b3f963ebe6fb565f9d37874cb3654e5
berkeley platte <berkeleyplatte989@gmail.com>
2020-04-11 14:50:22 -0500

    HackerRank - Fun With Anagrams

--- fun_with_anagrams.py
+++ fun_with_anagrams.py
@@ -10,15 +10,7 @@ string3 = ['code', 'ecod', 'anagrams', 'aanmgrsa']
 def fun_with_anagrams(text):
     for i in range(len(text)):
         try:
-            j = 1
-            # you always want to start with looking at
-            # the second index in the list
-            if i >= j:
-                # this ensures that program is never having a word
-                # look at itself, and allows me to check a word
-                # against every word in the list before moving
-                # on
-                j = i + 1
+            j = i + 1
             while j < len(text):
                 if sorted(text[i]) == sorted(text[j]):
                     text.remove(text[j])
